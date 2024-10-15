a4d975761468e6013f394bd67ed1c1bd006e36b5
Lami47 <liamrosenberg4777@gmail.com>
2024-10-15 10:51:24 +0200

    Prep for migration to spa

---
+++ src/userData.js
@@ -0,0 +1,44 @@
+const userData = [
+    {
+      name: "Liam",
+      surname: "Rosenberg",
+      username: "J0hndoo",
+      password:"qwerty12",
+      layout: " ",
+      id: 1
+    },
+    {
+      name: "John",
+      surname: "Doe",
+      username: "J0hndoo",
+      password:"qwerty12",
+      layout: " ",
+      id: 1
+    },
+    {
+      name: "John",
+      surname: "Doe",
+      username: "J0hndoo",
+      password:"qwerty12",
+      layout: " ",
+      id: 1
+    },
+    {
+      name: "John",
+      surname: "Doe",
+      username: "J0hndoo",
+      password:"qwerty12",
+      layout: " ",
+      id: 1
+    },
+    {
+      name: "John",
+      surname: "Doe",
+      username: "J0hndoo",
+      password:"qwerty12",
+      layout: " ",
+      id: 1
+    },
+  ]
+
+export default userData
